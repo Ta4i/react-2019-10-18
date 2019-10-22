9401d03719ac7d6b3388d912cb17d3eb9a9d6e08
Georgy Chernyshov <georgy.chernyshov@gmail.com>
2019-10-22 23:06:19 +0300

    Adding Reviews component to Restaurant component

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {Typography, Rate} from 'antd'
 import Menu from '../menu'
 import ReviewForm from '../review-form'
+import Reviews from '../reviews'
 
 class Restaurant extends Component {
   render() {
@@ -11,6 +12,7 @@ class Restaurant extends Component {
         <Typography.Title level={2}>{restaurant.name}</Typography.Title>
         <ReviewForm />
         <Menu menu={restaurant.menu} />
+        <Reviews reviews={restaurant.reviews} />
       </div>
     )
   }
