aac4d44a747ac5583d6291368765861571708865
Georgy Chernyshov <georgy.chernyshov@gmail.com>
2019-10-22 23:04:24 +0300

    Creating Reviews component based on antd.List

--- src/components/reviews/index.js
+++ src/components/reviews/index.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import {List, Typography} from 'antd'
 
 function Reviews(props) {
-  return <div>Reviews</div>
+  const {reviews} = props
+
+  return (
+    <List
+      header={<strong>Reviews</strong>}
+      footer={<strong>Total: {reviews.length}</strong>}
+      bordered
+      dataSource={reviews}
+      renderItem={item => (
+        <List.Item>
+          <Typography.Text mark>{item.user}</Typography.Text>
+          {': '}
+          {item.text}
+        </List.Item>
+      )}
+    />
+  )
 }
 
 export default Reviews
