268ba0dd58e171e96172a533c8d141e52ac89c6d
Mikhail Pastukhov <pastukhov.mi@gmail.com>
2019-10-24 00:32:37 +0300

    HT1 first hometask done

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -2,15 +2,28 @@ import React, {Component} from 'react'
 import {Typography, Rate} from 'antd'
 import Menu from '../menu'
 import ReviewForm from '../review-form'
+import DisabledRating from '../disabled-rating'
+import Reviews from '../reviews'
+
+const calcAvgRatingByReviews = (reviews) => {
+  const sum = reviews.reduce((sum, review) => {
+    return sum + review.rating
+  }, 0)
+
+  return sum / reviews.length
+}
 
 class Restaurant extends Component {
   render() {
     const {restaurant} = this.props
+
     return (
       <div>
         <Typography.Title level={2}>{restaurant.name}</Typography.Title>
-        <ReviewForm />
-        <Menu menu={restaurant.menu} />
+        <DisabledRating rating={calcAvgRatingByReviews(restaurant.reviews)}/>
+        <Menu menu={restaurant.menu}/>
+        <Reviews reviews={restaurant.reviews}/>
+        <ReviewForm/>
       </div>
     )
   }
