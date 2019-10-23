fce7921a030358c94ec6b006726236709078fb9d
andrey_platonov <xottabbich@mail.ru>
2019-10-23 22:57:08 +0600

    Add average rating for restaurants

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -6,9 +6,17 @@ import ReviewForm from '../review-form'
 class Restaurant extends Component {
   render() {
     const {restaurant} = this.props
+    const amountRating = restaurant.reviews.reduce(
+      (acc, review) => acc + review.rating,
+      0
+    )
+    const averageRating = amountRating
+      ? amountRating / restaurant.reviews.length
+      : 0
     return (
       <div>
         <Typography.Title level={2}>{restaurant.name}</Typography.Title>
+        <Rate disabled defaultValue={averageRating} />
         <ReviewForm />
         <Menu menu={restaurant.menu} />
       </div>
