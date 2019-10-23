9565699311b4dd4b16cbcadd095f1ccea9e68f99
Ivan S. Aksenov <aksenov.vanya@yandex.ru>
2019-10-23 20:55:56 +0300

    ratind

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -6,10 +6,18 @@ import ReviewForm from '../review-form'
 class Restaurant extends Component {
   render() {
     const {restaurant} = this.props
+    let rating = 0
+    let arr = []
+    for (let i = 0; i < restaurant.reviews.length; i++) {
+      arr.push(restaurant.reviews[i].rating)
+    }
+    rating = Math.round(arr.reduce((acc, c) => acc + c, 0) / arr.length)
     return (
       <div>
         <Typography.Title level={2}>{restaurant.name}</Typography.Title>
         <ReviewForm />
+
+        <Rate value={rating} />
         <Menu menu={restaurant.menu} />
       </div>
     )
