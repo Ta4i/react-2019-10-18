e97553b989ac4ebb53493ff52912cdf825a7dfbc
Sergey Smolin <SmolinSV@gmail.com>
2019-10-23 22:11:54 +0300

    task1 Rating

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -6,9 +6,15 @@ import ReviewForm from '../review-form'
 class Restaurant extends Component {
   render() {
     const {restaurant} = this.props
+    const reviews = restaurant.reviews
+    const rate =
+      reviews.reduce((prev, cur) => prev + cur.rating, 0) / reviews.length
     return (
       <div>
-        <Typography.Title level={2}>{restaurant.name}</Typography.Title>
+        <Typography.Title level={2}>
+          {restaurant.name}
+          <Rate allowHalf disabled value={rate} />
+        </Typography.Title>
         <ReviewForm />
         <Menu menu={restaurant.menu} />
       </div>
