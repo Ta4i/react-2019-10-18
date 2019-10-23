f6b90758cc475418d72d061c5abeac6dc8b90bd2
Ivan S. Aksenov <aksenov.vanya@yandex.ru>
2019-10-23 21:51:17 +0300

    first task

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -7,18 +7,24 @@ class Restaurant extends Component {
   render() {
     const {restaurant} = this.props
     let rating = 0
-    let arr = []
+    let arrRating = [],
+      arrReport = []
     for (let i = 0; i < restaurant.reviews.length; i++) {
-      arr.push(restaurant.reviews[i].rating)
+      arrRating.push(restaurant.reviews[i].rating)
+      arrReport.push(restaurant.reviews[i].text)
     }
-    rating = Math.round(arr.reduce((acc, c) => acc + c, 0) / arr.length)
+    rating = Math.round(
+      arrRating.reduce((acc, c) => acc + c, 0) / arrRating.length
+    )
+    const listReviews = arrReport.map(text => <li>{text}</li>)
     return (
       <div>
         <Typography.Title level={2}>{restaurant.name}</Typography.Title>
         <ReviewForm />
-
         <Rate value={rating} />
         <Menu menu={restaurant.menu} />
+        <p>Reviews:</p>
+        <ol>{listReviews}</ol>
       </div>
     )
   }
