c27799fe5a822431a1badcecceebdca5b6c2c514
Alena Katkova <akolpakova91@gmail.com>
2019-10-24 17:16:35 +0200

    add rating to restaurants

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react'
 import {Typography, Rate} from 'antd'
 import Menu from '../menu'
 import ReviewForm from '../review-form'
+import calcObjPropertyAverageInArr from '../../utils/calc-obj-property-average-in-arr'
+import roundToHalf from '../../utils/round-to-half'
 
 class Restaurant extends Component {
   render() {
     const {restaurant} = this.props
+    const rating = calcObjPropertyAverageInArr(restaurant.reviews, 'rating')
+    const roundedRating = roundToHalf(rating)
+
     return (
       <div>
         <Typography.Title level={2}>{restaurant.name}</Typography.Title>
+        <Rate value={roundedRating} allowHalf />
         <ReviewForm />
         <Menu menu={restaurant.menu} />
       </div>
