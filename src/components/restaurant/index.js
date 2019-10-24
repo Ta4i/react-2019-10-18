d23cc0bf63d05dc494e052e75c0d0cb394afe260
gLuneva <ga_lina19@mail.ru>
2019-10-24 22:14:15 +0300

    1 ДЗ - вывод рейтинга ввиде звездочек и вывод списка отзывов. 3 пункт не успела

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -2,18 +2,37 @@ import React, {Component} from 'react'
 import {Typography, Rate} from 'antd'
 import Menu from '../menu'
 import ReviewForm from '../review-form'
-
+import Reviews from '../reviews'
 class Restaurant extends Component {
   render() {
     const {restaurant} = this.props
     return (
       <div>
         <Typography.Title level={2}>{restaurant.name}</Typography.Title>
+        <Rate
+          allowHalf
+          disabled
+          defaultValue={calculateMiddleRate(restaurant)}
+        />
         <ReviewForm />
+        <Typography.Title level={3}>Reviews</Typography.Title>
+        <Reviews reviews={restaurant.reviews} />
         <Menu menu={restaurant.menu} />
       </div>
     )
   }
+}
+
+function calculateMiddleRate(rest) {
+  var rating = 0
+  if (rest.reviews.length) {
+    for (var i = 0; i < rest.reviews.length; i++) {
+      rating += rest.reviews[i].rating
+    }
+    console.log(rating / rest.reviews.length)
+    return rating / rest.reviews.length
+  }
+  return 0
 }
 
 export default Restaurant
