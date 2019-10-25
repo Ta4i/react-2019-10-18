c31c79945a91356983bda8f84ac7fbbf5a26739c
Dmitiry Shulga <dev.dmitriy@gamil.com>
2019-10-25 13:58:11 +0300

    rating

---
+++ src/components/rating/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import {Rate} from 'antd'
+
+function Rating(props) {
+  const {reviews} = props
+  let rating = getRating(reviews)
+
+  return (
+    <div>
+      <Rate allowHalf defaultValue={rating} />
+    </div>
+  )
+}
+
+function getRating(reviews) {
+  let sum = 0
+  reviews.forEach(review => (sum += +review.rating))
+  return sum / reviews.length
+}
+
+export default Rating
--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -2,13 +2,17 @@ import React, {Component} from 'react'
 import {Typography, Rate} from 'antd'
 import Menu from '../menu'
 import ReviewForm from '../review-form'
+import Rating from '../rating'
 
 class Restaurant extends Component {
   render() {
     const {restaurant} = this.props
+    const reviews = restaurant.reviews
+    console.log(reviews)
     return (
       <div>
         <Typography.Title level={2}>{restaurant.name}</Typography.Title>
+        <Rating reviews={restaurant.reviews} />
         <ReviewForm />
         <Menu menu={restaurant.menu} />
       </div>
