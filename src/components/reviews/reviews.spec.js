66fed5ebd6429d33060ec599d28182536278a253
Georgy Chernyshov <georgy.chernyshov@gmail.com>
2019-10-26 18:16:56 +0300

    Adding test cases for review component - assert is array - assert is reviews count equals 2 - assert users name contains in reviews - assert text contains in reviews

--- src/components/reviews/reviews.spec.js
+++ src/components/reviews/reviews.spec.js
@@ -2,11 +2,36 @@ import React from 'react'
 import Enzyme, {mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Reviews from './reviews'
+import {restaurants} from '../../fixtures'
 
 Enzyme.configure({adapter: new Adapter()})
 
 describe('Reviews', function() {
   it('show call fetchReviews if it is provided', function(done) {
     mount(<Reviews reviews={[]} fetchReviews={() => done()} />)
+  })
+
+  const [{reviews: actualReviews}] = restaurants
+  const wrapper = mount(<Reviews reviews={actualReviews} />)
+  const expectedReviews = wrapper.find('[data-automation-id="REVIEWS"]')
+
+  it('assert is array', function() {
+    expect.arrayContaining(expectedReviews)
+  })
+
+  it('assert is reviews count equals 2', function() {
+    expect(expectedReviews.length).toBe(2)
+  })
+
+  it('assert users name contains in reviews', function() {
+    expectedReviews.forEach((item, index) => {
+      expect(item.find('Title').text()).toBe(actualReviews[index].user)
+    })
+  })
+
+  it('assert text contains in reviews', function() {
+    expectedReviews.forEach((item, index) => {
+      expect(item.find('Text').text()).toBe(actualReviews[index].text)
+    })
   })
 })
