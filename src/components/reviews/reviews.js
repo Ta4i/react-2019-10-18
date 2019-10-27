f2cd4c84d9a4d1db7487c7543e0e39ca801c1d86
andrey_platonov <xottabbich@mail.ru>
2019-10-27 22:00:12 +0600

    Write test for reviews component

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -8,12 +8,22 @@ function Reviews({reviews, fetchReviews} /*props*/) {
     fetchReviews && fetchReviews()
   }
   useEffect(foo, [])
+  // Can't understand why this data attribute doesn't work like on antd components
+  // So I moved it in review component
+  const reviewsListContent = reviews.length ? (
+    reviews.map(review => (
+      <Review review={review} key={review.id} data-automation-id="TEST" />
+    ))
+  ) : (
+    <div data-automation-id="REVIEW_NO_CONTENT_MSG">
+      There are no reviews for now. Be first and create your own using form
+      underneath.
+    </div>
+  )
   return (
     <Row type="flex" justify="center" gutter={{xs: 8, sm: 16, md: 24}}>
       <Col xs={24} md={16}>
-        {reviews.map(review => (
-          <Review review={review} key={review.id} />
-        ))}
+        {reviewsListContent}
         <ReviewForm />
       </Col>
     </Row>
--- src/components/reviews/reviews.spec.js
+++ src/components/reviews/reviews.spec.js
@@ -2,11 +2,28 @@ import React from 'react'
 import Enzyme, {mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Reviews from './reviews'
+import {restaurants} from '../../fixtures'
 
 Enzyme.configure({adapter: new Adapter()})
 
 describe('Reviews', function() {
   it('show call fetchReviews if it is provided', function(done) {
     mount(<Reviews reviews={[]} fetchReviews={() => done()} />)
+  })
+
+  it('Check count of reviews', function() {
+    const reviewsData = restaurants[0].reviews
+    const wrapper = mount(<Reviews reviews={reviewsData} />)
+    expect(wrapper.find('div[data-automation-id="REVIEW_ITEM"]').length).toBe(
+      reviewsData.length
+    )
+  })
+
+  it('Check having proper warning for no content case', function() {
+    const reviewsData = []
+    const wrapper = mount(<Reviews reviews={reviewsData} />)
+    expect(
+      wrapper.find('div[data-automation-id="REVIEW_NO_CONTENT_MSG"]').length
+    ).toBe(1)
   })
 })
