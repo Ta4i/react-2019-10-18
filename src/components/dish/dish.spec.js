e80f41502584e55f824a7fa68203ec637869f813
Aidar Haiatov <aidar.haiatov@gmail.com>
2019-10-28 09:15:32 +0300

    task 3 done (dish decrease test added)

--- src/components/dish/dish.spec.js
+++ src/components/dish/dish.spec.js
@@ -6,6 +6,13 @@ import {restaurants} from '../../fixtures'
 
 Enzyme.configure({adapter: new Adapter()})
 
+function triggerClickBySelector(wrapper, selector) {
+  wrapper
+    .find(selector)
+    .first()
+    .simulate('click')
+}
+
 describe('Dish', function() {
   it('show name', function() {
     const dishData = restaurants[0].menu[0]
@@ -21,15 +28,53 @@ describe('Dish', function() {
   it('when click on Plus button it should increase amount', function() {
     const dishData = restaurants[0].menu[0]
     const wrapper = mount(<Dish dish={dishData} />)
-    wrapper
-      .find('button[data-automation-id="INCREASE"]')
-      .first()
-      .simulate('click')
+
+    triggerClickBySelector(wrapper, 'button[data-automation-id="INCREASE"]')
+
     expect(
       wrapper
         .find('div[data-automation-id="AMOUNT"]')
         .first()
         .text()
     ).toBe('1')
+  })
+})
+
+describe('Decrease amount of dish in cart', function() {
+  it('Positive value', function() {
+    const dishData = restaurants[0].menu[0]
+    const wrapper = mount(<Dish dish={dishData} />)
+
+    triggerClickBySelector(wrapper, 'button[data-automation-id="INCREASE"]')
+    triggerClickBySelector(wrapper, 'button[data-automation-id="INCREASE"]')
+    triggerClickBySelector(wrapper, 'button[data-automation-id="DECREASE"]')
+    expect(
+      wrapper
+        .find('div[data-automation-id="AMOUNT"]')
+        .first()
+        .text()
+    ).toBe('1')
+
+    triggerClickBySelector(wrapper, 'button[data-automation-id="DECREASE"]')
+    expect(
+      wrapper
+        .find('div[data-automation-id="AMOUNT"]')
+        .first()
+        .text()
+    ).toBe('0')
+  })
+
+  it('Zero value', function() {
+    const dishData = restaurants[0].menu[0]
+    const wrapper = mount(<Dish dish={dishData} />)
+
+    triggerClickBySelector(wrapper, 'button[data-automation-id="DECREASE"]')
+
+    expect(
+      wrapper
+        .find('div[data-automation-id="AMOUNT"]')
+        .first()
+        .text()
+    ).toBe('0')
   })
 })
