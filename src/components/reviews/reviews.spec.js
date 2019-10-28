9205428cce9eeeb90a0519c3945ebaef1b6f7a8a
gLuneva <ga_lina19@mail.ru>
2019-10-28 20:58:34 +0300

    HN2 - 1. tests for review

--- src/components/reviews/reviews.spec.js
+++ src/components/reviews/reviews.spec.js
@@ -3,6 +3,7 @@ import Enzyme, {mount} from 'enzyme'
 import {restaurants} from '../../fixtures'
 import Adapter from 'enzyme-adapter-react-16'
 import Reviews from './reviews'
+import ReviewForm from '../review-form'
 
 Enzyme.configure({adapter: new Adapter()})
 
@@ -11,9 +12,18 @@ describe('Reviews', function() {
     mount(<Reviews reviews={[]} fetchReviews={() => done()} />)
   })
 
-  it('reviews list should has review-form', function() {
+  it('reviews list should has review item', function() {
     const reviews = restaurants[0].reviews
+    const reviewsCount = reviews.length
     const wrapper = mount(<Reviews reviews={reviews} />)
-    expect(wrapper.find('[data-automation-id="REVIEWS"]'))
+    expect(wrapper.find('[data-automation-id="REVIEW_ITEM"]').length).toBe(
+      reviewsCount
+    )
+  })
+
+  it('reviews list should has review form', function() {
+    //const reviews = restaurants[0].reviews
+    const wrapper = mount(<Reviews reviews={undefined} />)
+    expect(wrapper.find('[data-automation-id="REVIEW_FORM"]').length).toBe(1)
   })
 })
