0f7522f0b1a0b5973ce91591df0ab83167dab221
Ekaterina Ganshina <ekaterina.ganshina@bk.ru>
2019-10-28 21:40:58 +0300

    Add tests for Reviews component

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -10,7 +10,7 @@ function Reviews({reviews, fetchReviews} /*props*/) {
   useEffect(foo, [])
   return (
     <Row type="flex" justify="center" gutter={{xs: 8, sm: 16, md: 24}}>
-      <Col xs={24} md={16}>
+      <Col xs={24} md={16} data-automation-id="REVIEW_CONTAINER">
         {reviews.map(review => (
           <Review review={review} key={review.id} />
         ))}
--- src/components/reviews/reviews.spec.js
+++ src/components/reviews/reviews.spec.js
@@ -2,11 +2,49 @@ import React from 'react'
 import Enzyme, {mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Reviews from './reviews'
+import {restaurants} from '../../fixtures'
+import Review from './review'
+import ReviewForm from '../review-form'
 
 Enzyme.configure({adapter: new Adapter()})
 
 describe('Reviews', function() {
   it('show call fetchReviews if it is provided', function(done) {
     mount(<Reviews reviews={[]} fetchReviews={() => done()} />)
+  })
+
+  it('should show the same number of reviews as in data', function() {
+    const data = restaurants[0].reviews
+    const wrapper = mount(<Reviews reviews={data} />)
+
+    expect(wrapper.find(Review)).toHaveLength(data.length)
+  })
+
+  it('should render Review form if data is empty', function() {
+    const wrapper = mount(<Reviews reviews={[]} />)
+
+    expect(wrapper.find(ReviewForm)).toHaveLength(1)
+  })
+
+  it('should render Review form if data is undefined', function() {
+    const wrapper = mount(<Reviews />)
+
+    expect(wrapper.find(ReviewForm)).toHaveLength(1)
+  })
+
+  it('should render Review form as the last element in container', function() {
+    const data = restaurants[0].reviews
+    const wrapper = mount(<Reviews reviews={data} />)
+
+    expect(wrapper.find(ReviewForm)).toHaveLength(1)
+    expect(
+      wrapper.find('div[data-automation-id="REVIEW_CONTAINER"]').children()
+    ).toHaveLength(data.length + 1) // reviews + review form
+    expect(
+      wrapper
+        .find('div[data-automation-id="REVIEW_CONTAINER"]')
+        .childAt(data.length)
+        .getElement()
+    ).toEqual(<ReviewForm />)
   })
 })
