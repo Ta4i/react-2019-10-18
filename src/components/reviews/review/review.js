57f60de9c7c794c2cbb4b9e5236f0a2713bd8f30
Ivan <pautovweb@gmail.com>
2019-10-28 21:24:14 -0700

    Add [data-automation-id] tags for testing

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -6,10 +6,17 @@ const Review = ({review}) => (
   <Card className={styles.review}>
     <Row type="flex" align="middle">
       <Col xs={24} md={18} align="left">
-        <Typography.Title className={styles.name} level={4}>
+        <Typography.Title
+          className={styles.name}
+          level={4}
+          data-automation-id="REVIEW_USER"
+        >
           {review.user}
         </Typography.Title>
-        <Typography.Text className={styles.comment}>
+        <Typography.Text
+          className={styles.comment}
+          data-automation-id="REVIEW_TEXT"
+        >
           {review.text}
         </Typography.Text>
       </Col>
