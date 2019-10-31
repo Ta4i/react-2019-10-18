f8be8ca752ff39dca73e4ea72e7e6e6195cbbe60
Alena Katkova <akolpakova91@gmail.com>
2019-10-31 15:10:50 +0100

    add cart items decrement

--- src/store/reducer/cart.js
+++ src/store/reducer/cart.js
@@ -1,11 +1,24 @@
 export const cartReducer = (cartState = {}, action) => {
-  if (action.type === 'ADD_TO_CART') {
-    const {dishId} = action.payload
-    const currentAmount = cartState[dishId] || 0
-    return {
-      ...cartState,
-      [dishId]: currentAmount + 1,
+  switch (action.type) {
+    case 'ADD_TO_CART': {
+      const {dishId} = action.payload
+      const currentAmount = cartState[dishId] || 0
+
+      return {
+        ...cartState,
+        [dishId]: currentAmount + 1,
+      }
     }
+    case 'REMOVE_FROM_CART': {
+      const {dishId} = action.payload
+      const currentAmount = cartState[dishId] || 0
+
+      return {
+        ...cartState,
+        [dishId]: currentAmount > 0 && currentAmount - 1,
+      }
+    }
+    default:
+      return cartState
   }
-  return cartState
 }
