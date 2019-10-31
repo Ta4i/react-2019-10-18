4b2c3b840672e1494aa946f007cf71e3aa4dd826
Serhii <sergey@coderiver.com.ua>
2019-10-31 10:01:17 +0200

    add decrease dish from store

--- src/store/reducer/cart.js
+++ src/store/reducer/cart.js
@@ -7,5 +7,13 @@ export const cartReducer = (cartState = {}, action) => {
       [dishId]: currentAmount + 1,
     }
   }
+  if (action.type === 'REMOVE_FROM_CART') {
+    const {dishId} = action.payload
+    const currentAmount = cartState[dishId] || 0
+    return {
+      ...cartState,
+      [dishId]: currentAmount - 1 > 0 ? currentAmount - 1 : 0,
+    }
+  }
   return cartState
 }
