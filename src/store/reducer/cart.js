4746dd9cb257b45b7fa6cdb3d7d0b970976dd5fc
DenisSkachko <denix.sg@gmail.com>
2019-11-08 02:33:25 +0300

    fix

--- src/store/reducer/cart.js
+++ src/store/reducer/cart.js
@@ -2,18 +2,20 @@ export const cartReducer = (cartState = {}, action) => {
   if (action.type === 'ADD_TO_CART') {
     const {dishId} = action.payload
     const currentAmount = cartState[dishId] || 0
+    const amount = currentAmount + 1
     return {
       ...cartState,
-      [dishId]: currentAmount + 1,
+      [dishId]: amount,
     }
   }
 
   if (action.type === 'REMOVE_FROM_CART') {
     const {dishId} = action.payload
     const currentAmount = cartState[dishId] || 0
+    const amount = currentAmount > 1 ? currentAmount - 1 : 0
     return {
       ...cartState,
-      [dishId]: currentAmount > 1 ? currentAmount - 1 : 0,
+      [dishId]: amount,
     }
   }
 
