b3d47a6df77a5a31625ed116e644f5e54d8e24e9
Dmitiry Shulga <dev.dmitriy@gamil.com>
2019-11-15 17:55:15 +0200

    rewrite to component

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,40 +1,49 @@
-import React, {useEffect} from 'react'
+import React, {Component} from 'react'
 import ReviewForm from '../review-form'
 import Review from './review'
 import {Col, Row} from 'antd'
 import PropTypes from 'prop-types'
 import {selectReviews} from '../../store/selectors'
-import {useSelector} from 'react-redux'
+import {connect} from 'react-redux'
 
-function Reviews({id, fetchReviews}) {
-  const reviews = useSelector(state => selectReviews(state, {id}))
-  useEffect(() => {
-    fetchReviews && fetchReviews()
-  }, [fetchReviews])
-  return (
-    <Row type="flex" justify="center" gutter={{xs: 8, sm: 16, md: 24}}>
-      <Col xs={24} md={16}>
-        {reviews.map(review => (
-          <Review
-            review={review}
-            key={review.id}
-            data-automation-id={`REVIEW_${review.id}`}
-          />
-        ))}
-        <ReviewForm id={id} />
-      </Col>
-    </Row>
-  )
-}
+class Reviews extends Component {
+  static defaultProps = {
+    reviews: [],
+  }
 
-Reviews.defaultProps = {
-  reviews: [],
+  componentDidMount() {
+    console.log('component mounnted')
+  }
+
+  render() {
+    const {reviews, id} = this.props
+    console.log('rendering', reviews)
+
+    return (
+      <Row type="flex" justify="center" gutter={{xs: 8, sm: 16, md: 24}}>
+        <Col xs={24} md={16}>
+          {reviews.map(review => (
+            <Review
+              review={review}
+              key={review.id}
+              data-automation-id={`REVIEW_${review.id}`}
+            />
+          ))}
+          <ReviewForm id={id} />
+        </Col>
+      </Row>
+    )
+  }
 }
 
 Reviews.propTypes = {
   id: PropTypes.string.isRequired,
-  reviews: PropTypes.arrayOf(PropTypes.string).isRequired,
+  reviews: PropTypes.arrayOf(PropTypes.object),
   fetchReviews: PropTypes.func,
 }
 
-export default Reviews
+const mapStateToProps = (state, ownProps) => ({
+  reviews: selectReviews(state, ownProps),
+})
+
+export default connect(mapStateToProps)(Reviews)
